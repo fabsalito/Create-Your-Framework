10ff94353cd2ee8b520cf16e314754af8620615e
fabsalito <fabricio.salinas@gmail.com>
2013-04-30 08:17:18 -0500

    Finished part 3.-

--- src/autoload.php
+++ src/autoload.php
@@ -3,7 +3,7 @@
 // framework/autoload.php
  
 //require_once __DIR__.'/vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
  
 use Symfony\Component\ClassLoader\UniversalClassLoader;
  
