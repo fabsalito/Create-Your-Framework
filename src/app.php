40395832c9b30aacdbdfe73f543df485ab9878c3
fabsalito <fabricio.salinas@gmail.com>
2013-05-13 16:48:47 -0500

    Finished part 5.-

--- src/app.php
+++ src/app.php
@@ -2,10 +2,49 @@
  
 // example.com/src/app.php
  
+ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
+
+function is_leap_year($year = null) {
+    if (null === $year) {
+        $year = date('Y');
+    }
  
+    return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
+}
+
+// crea una nueva coleeción de rutas
 $routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'World')));
-$routes->add('bye', new Routing\Route('/bye'));
+
+// agrega ruta para /hello/{name}
+$routes->add('hello', new Routing\Route('/hello/{name}', array(
+    'name' => 'World',
+    '_controller' => function($request){
+        return render_template($request);
+    }
+)));
+
+// agrega ruta para /bye
+$routes->add('bye', new Routing\Route('/bye', array(
+    '_controller' => function($request){
+        return render_template($request);
+    }
+)));
+
+// agrega ruta para is_leap_year/{year}
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
+    'year' => null,
+    '_controller' => function ($request) {
+        if (is_leap_year($request->attributes->get('year'))) {
+            $response = new Response('Yep, this is a leap year!');
+
+            return $response;
+        }
+ 
+        $response = new Response('Nope, this is not a leap year.');
+
+        return $response;
+    }
+)));
  
 return $routes;
--- web/front.php
+++ web/front.php
@@ -7,6 +7,15 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
+
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+ 
+    return new Response(ob_get_clean());
+}
  
 // crea petición desde globales PHP
 $request = Request::createFromGlobals();
@@ -22,17 +31,11 @@ $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
  
 try {
-    // carga pares de la ruta coincidente a la tabla de simbolos actual
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    
-    // activa almacenamiento en buffer
-    ob_start();
+    // añade a los atributos de la petición la información del path
+    $request->attributes->add($matcher->match($request->getPathInfo()));
 
-    // carga la página indicada ($_route está cargada en la tabla de símbolos)
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-    
-    // crea respuesta en base al contenido del buffer
-    $response = new Response(ob_get_clean());
+    // llama a render_template pasando la petición como parámetro
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 
 } catch (Routing\Exception\ResourceNotFoundException $e) {
     // crea respuesta para página no encontrada
@@ -41,7 +44,10 @@ try {
 } catch (Exception $e) {
     // crea respuesta para excepción
     $response = new Response('An error occurred', 500);
+
 }
+
+//var_dump($response);die;
  
 // envía respuesta
 $response->send();
